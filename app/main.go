832ebb16e4c3033a59c38ffc76b14f5c5dd500c3
Xavier Roma <x.roma@pm.me>
2025-04-26 17:10:28 +0100

    handle connection

--- app/main.go
+++ app/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"os"
+	"time"
 )
 
 func main() {
@@ -15,9 +17,44 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	for {
+		fmt.Println("Waiting for connection...")
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("Connection accepted")
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		go func() {
+			handler(ctx, conn)
+		}()
+	}
+}
+
+func handler(ctx context.Context, conn net.Conn) {
+	fmt.Println("Handling request...")
+	handleRequest(ctx, conn)
+	fmt.Println("Request handled")
+	fmt.Println("Handling response...")
+	handleResponse(ctx, conn)
+	fmt.Println("Response handled")
+}
+
+func handleRequest(ctx context.Context, conn net.Conn) {
+	request := make([]byte, 1024)
+	_, err := conn.Read(request)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		fmt.Println("Error reading request: ", err.Error())
 		os.Exit(1)
 	}
+
+	fmt.Println(string(request))
+}
+
+func handleResponse(ctx context.Context, conn net.Conn) {
+	response := "HTTP/1.1 200 OK\r\n\r\n"
+	conn.Write([]byte(response))
+	conn.Close()
 }
